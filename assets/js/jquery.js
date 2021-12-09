ca7441c086a7e93dbc1b1eb5444bdeb4886dada2
Katherine Vergara <estudio.8319@gmail.com>
2021-12-09 04:34:12 -0300

    jquery para scrollmooth

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,24 @@
+$("a").on('click', function(event) {
+
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1000, function(){
+
+      window.location.hash = hash;
+    });
+  }
+});
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
-})
+});
+
+
+
 
 
